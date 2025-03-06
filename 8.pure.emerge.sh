85c0cd9aceac04929b38a81c29e399bc4ba1b375
Ivan I. Grushin <mcgru@cgames.team>
2025-03-06 16:52:38 +0700

    add 8.pure.emerge, set trap on exit n z.

--- 8.pure.emerge.sh
+++ 8.pure.emerge.sh
@@ -3,7 +3,14 @@
 set -euo pipefail
 set -a ;THIS=$(realpath -P $0); CWD=$(dirname "$THIS"); source "$CWD"/common.conf; [ -r "${THIS%.sh}.conf" ] && source "${THIS%.sh}.conf";  set +a
 
-trap "$CWD/9.umount.sh" EXIT INT QUIT TERM KILL
+cd $FSDIR
+set -x
 
-$CWD/2.mount.sh
-$CWD/4.emerge-world.sh "$@"
+CMD="emerge"
+TGT=""
+[ "$#" -gt 0 ] && TGT="$@" ||:
+chroot merged /bin/bash -c "$CMD $TGT"
+
+
+echo 1>&2 "OK: finished  $0 $*"
+exit
