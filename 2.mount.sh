961f06ca52347b080cb402da6a774be119d965db
Ivan I. Grushin <mcgru@cgames.team>
2025-03-06 11:53:40 +0700

    add common.conf.varlib and chmod 700 FSDIR

--- 2.mount.sh
+++ 2.mount.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 set -a ;THIS=$(realpath -P $0); CWD=$(dirname "$THIS"); source "$CWD"/common.conf; [ -r "${THIS%.sh}.conf" ] && source "${THIS%.sh}.conf";  set +a
 
 : ${FSTAB:=this.fstab}
-mkdir -p $FSDIR
+mkdir  -p "$FSDIR"
+chmod 700 "$FSDIR"
 
 create_some_mountpoints(){
 ( cd $FSDIR
