cad5dcd1d42da867f8e8a794729fcaf64c4ac59b
Ivan I. Grushin <mcgru@cgames.team>
2025-03-06 12:14:39 +0700

    add z.full(cycle).emerge-world script

---
+++ z.full.emerge-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+set -a ;THIS=$(realpath -P $0); CWD=$(dirname "$THIS"); source "$CWD"/common.conf; [ -r "${THIS%.sh}.conf" ] && source "${THIS%.sh}.conf";  set +a
+
+$CWD/2.mount.sh
+$CWD/4.emerge-world.sh
+$CWD/9.umount.sh
